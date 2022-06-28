e5b1d64a5db380d3db23ba741ed85649c198b3d2
habu27 <100352651+habu27@users.noreply.github.com>
2022-06-28 12:15:04 -0700

    task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-LockedClass = __import__('101-locked_class').LockedClass
+"""
+    101-locked_class: class LockedClass
+"""
 
-lc = LockedClass()
-lc.first_name = "John"
-try:
-    lc.last_name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+
+class LockedClass:
+    """
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
+    __slots__ = ['first_name']
